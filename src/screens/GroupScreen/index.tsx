dddf2b6708f95b46a51bdcbc49f1c742067b6f33
fernandolucasgsouza <fernandolucass.2012@gmail.com>
2024-03-28 08:40:23 -0300

    cleancode

--- src/screens/GroupScreen/index.tsx
+++ src/screens/GroupScreen/index.tsx
@@ -2,8 +2,6 @@ import { StatusBar } from "expo-status-bar";
 import { Container, Title } from "./styles";
 import { HeaderComponent } from "@components/HeaderComponent";
 
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import { HighlightComponent } from "@components/HighlightComponet";
 
 export function GroupScreen() {
