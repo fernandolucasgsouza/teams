9679a3504a04fe379174ad5e10e75c5d687e2c78
fernandolucasgsouza <fernandolucass.2012@gmail.com>
2024-04-05 08:13:19 -0300

    code reorganization

--- src/components/InputComponent/index.tsx
+++ src/components/InputComponent/index.tsx
@@ -4,11 +4,9 @@ import { useTheme } from "styled-components/native";
 import { Container } from "./styles";
 
 export function InputComponent({ ...rest }: TextInputProps) {
-  const {COLORS} = useTheme()
+  const { COLORS } = useTheme();
 
-  return <Container 
-      {...rest}
-      placeholderTextColor={COLORS.GRAY_300}
-  >
-  </Container>;
+  return (
+    <Container {...rest} placeholderTextColor={COLORS.GRAY_300}></Container>
+  );
 }
